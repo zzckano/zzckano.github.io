0019e37b4d2161f8fb1f121a825d3ddcdb10cce2
Virkano <Virkano@163.comm>
2020-11-05 12:32:23 +0800

    v1

---
+++ docs/.vuepress/sidebarMap.js
@@ -0,0 +1,17 @@
+const sidebarMap = [
+  { title: 'HTML', dirname: 'base/html' },
+  { title: 'CSS', dirname: 'base/css'},
+  { title: 'JavaScript', dirname: 'base/javascript' },
+  { title: 'Color', dirname: 'base/color' },
+  { title: '正则表达式', dirname: 'base/regx' },
+  { title: '技术', dirname: 'tech' },
+  { title: '文章', dirname: 'article' },
+  { title: '工具', dirname: 'tool-site/tool' },
+  { title: '网站', dirname: 'tool-site/site' },
+  { title: '收藏', dirname: 'favorite' },
+  { title: '面试', dirname: 'interview' },
+  // { title: '圈子', dirname: 'other/gard' },
+  // { title: '其它', dirname: 'other/another' }
+]
+
+module.exports = sidebarMap
---
+++ docs/base/regx/reg1.md
@@ -0,0 +1,6 @@
+# 正则 API
+
+test
+
+[https://github.com/ziishaned/learn-regex/blob/master/translations/README-cn.md](https://github.com/ziishaned/learn-regex/blob/master/translations/README-cn.md)
+
